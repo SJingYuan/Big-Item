c073ce9dd5caf08167c5091bc63630c353cb9a83
JingYuan <JingYuanan_4@163.com>
2020-01-16 17:31:23 +0800

    用户信息

---
+++ web_back/js/util/user.js
@@ -0,0 +1,19 @@
+var Url = "http://192.168.172.82:8000";
+
+var user = {
+  //发送请求
+  login: function (name, password) {
+    return $.post(Url + "/admin/login", {
+      "user_name": name,
+      "password": password
+    })
+  },
+  //用户推出
+  logout: function () {
+    return $.post(Url + "/admin/logout")
+  },
+  //获取用户信息
+  getInfo: function () {
+    return $.get(Url + '/admin/getuser')
+  }
+};
