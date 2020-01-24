ec40698b2824b6dbe56fb43075390c7f1e7197a3
JingYuan <JingYuanan_4@163.com>
2020-01-24 14:01:56 +0800

    文章类型编辑，新增结束

--- web_back/js/util/wenzhang.js
+++ web_back/js/util/wenzhang.js
@@ -1,14 +1,19 @@
-var Url = "http://10.253.1.37:8000";
+// var Url = "http://192.168.172.56:8000";
+// var Url = "http://192.168.124.15:8000";
+var Url = "http://192.168.43.94:8000";
 var category = {
   //获取数据
   get: function () {
     return $.get(Url + "/admin/category_search")
   },
   //添加分类
-  add: function (name,slug) {
-    return $.post(Url + "/admin/category_add",{'name':name,'slug':slug})
+  add: function (name, slug) {
+    return $.post(Url + "/admin/category_add", { 'name': name, 'slug': slug })
   },
-  del:function(id){
-    return $.post(Url+"/admin/category_delete",{'id':id})
+  del: function (id) {
+    return $.post(Url + "/admin/category_delete", { 'id': id })
+  },
+  edit:function(id,name,slug){
+    return $.post(Url+"/admin/category_edit",{"id":id,"name":name,"slug":slug})
   }
 }
