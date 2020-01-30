855fd04f1040278fdd2e0dae7385689205a0503f
JingYuan <JingYuanan_4@163.com>
2020-01-30 16:02:00 +0800

    文章列表删除，和筛选功能完成

--- web_back/js/util/modify.js
+++ web_back/js/util/modify.js
@@ -1,12 +1,15 @@
 var Url = "http://localhost:8000";
 var list = {
-  obj: function (key, state) {
-    return $.get(Url + `/admin/search?key=${key}&state=${state}`)
-  },
-  filters: function () {
-    return $.get(Url + "/admin/search")
+  // obj: function (key, state, perpage) {
+  //   return $.get(Url + `/admin/search?key=${key}&state=${state}&perpage=${perpage}`)
+  // },
+  obj: function (isParams) {
+    return $.get(Url + `/admin/search?key=${isParams.name}&state=${isParams.slug}&perpage=${isParams.visiblecount}`)
   },
   search: function () {
     return $.get(Url + "/admin/category_search")
+  },
+  del:function(id){
+    return $.get(Url+`/admin/article_delete?id=${id}`)
   }
 }
